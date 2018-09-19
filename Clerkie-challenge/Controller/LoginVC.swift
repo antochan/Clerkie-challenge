bfcad17d96a3222c320b934c1c54ce88b0ca9c79
Antonio Chan <antonio.chan@emory.edu>
2018-09-19 03:30:03 -0400

    initial setup

---
+++ Clerkie-challenge/Controller/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  LoginVC.swift
+//  Clerkie-challenge
+//
+//  Created by Antonio Chan on 2018/9/19.
+//  Copyright © 2018 Antonio Chan. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}

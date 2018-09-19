867320fbf56ee9403f2961d8d002b34c8677a04e
Antonio Chan <antonio.chan@emory.edu>
2018-09-19 17:10:37 -0400

    More new initial files with initial auth flow

--- Clerkie-challenge/Controller/LoginVC.swift
+++ Clerkie-challenge/Controller/LoginVC.swift
@@ -7,13 +7,156 @@
 //
 
 import UIKit
+import Lottie
+import TextFieldEffects
+import TransitionButton
 
 class LoginVC: UIViewController {
-
+    
+    let loginButton = TransitionButton()
+    let signupButton = UIButton()
+    let passwordTextField = IsaoTextField()
+    let emailTextField = IsaoTextField()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpNavigationBar()
+        logoAnimate()
+        setUpEmailTextField()
+        setUpPasswordTextField()
+        setUpLoginButton()
+        setUpSignupButton()
+    }
+    
+    func logoAnimate() {
+        let animationView = LOTAnimationView(name: "Logo")
+        animationView.contentMode = .scaleAspectFill
+        
+        animationView.frame = CGRect(x:0, y: 40, width:240, height:240)
+        animationView.center.x = self.view.center.x
+        
+        
+        self.view.addSubview(animationView)
+        animationView.play{ (finished) in
+            //TODO: put label
+        }
+    }
+    
+    func setUpNavigationBar() {
+        
+        //clear navigation bar
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = .clear
+        
+        //back button
+        self.navigationController?.navigationBar.tintColor = UIColor.white
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style:.plain, target: nil, action: nil)
+        
+        navigationController?.navigationBar.isHidden = false
+        
+    }
+    
+    fileprivate func setUpEmailTextField() {
+        // email textfield
+        view.addSubview(emailTextField)
+        emailTextField.delegate = self
+        emailTextField.autocapitalizationType = UITextAutocapitalizationType.none
+        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        emailTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        emailTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 0).isActive = true
+        emailTextField.widthAnchor.constraint(equalToConstant: 280).isActive = true
+        emailTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        emailTextField.font = UIFont.avenirNextRegularFontOfSize(size: 15)
+        emailTextField.inactiveColor = .white
+        emailTextField.activeColor = UIColor.FlatColor.Yellow.PastelYellow
+        emailTextField.placeholder = "Email / Phone"
+        emailTextField.keyboardType = .emailAddress
+    }
+    
+    fileprivate func setUpPasswordTextField() {
+        //password text field
+        view.addSubview(passwordTextField)
+        passwordTextField.delegate = self
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        passwordTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 60).isActive = true
+        passwordTextField.widthAnchor.constraint(equalToConstant: 280).isActive = true
+        passwordTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        passwordTextField.font = UIFont.avenirNextRegularFontOfSize(size: 15)
+        passwordTextField.inactiveColor = .white
+        passwordTextField.activeColor = UIColor.FlatColor.Yellow.PastelYellow
+        passwordTextField.placeholder = "Password"
+        passwordTextField.isSecureTextEntry = true
+    }
+    
+    fileprivate func setUpLoginButton() {
+        //submit button
+        view.addSubview(loginButton)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 170).isActive = true
+        loginButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        loginButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        loginButton.titleLabel?.font = UIFont.avenirNextDemiBoldFontOfSize(size: 15)
+        loginButton.backgroundColor = .clear
+        loginButton.layer.borderWidth = 2
+        loginButton.layer.borderColor = UIColor.white.cgColor
+        loginButton.setTitle("Login", for: .normal)
+        loginButton.cornerRadius = 20
+        loginButton.spinnerColor = .white
+        loginButton.addTarget(self, action: #selector(initiateLogin(_:)), for: .touchUpInside)
+    }
+    
+    fileprivate func setUpSignupButton() {
+        //submit button
+        view.addSubview(signupButton)
+        signupButton.translatesAutoresizingMaskIntoConstraints = false
+        signupButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        signupButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 230).isActive = true
+        signupButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        signupButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        signupButton.titleLabel?.font = UIFont.avenirNextDemiBoldFontOfSize(size: 15)
+        signupButton.backgroundColor = .white
+        signupButton.setTitle("Signup", for: .normal)
+        signupButton.setTitleColor(UIColor.FlatColor.Blue.PastelBlue, for: .normal)
+        signupButton.layer.cornerRadius = 20
+        signupButton.addTarget(self, action: #selector(initiateSignupForm), for: .touchUpInside)
+    }
+    
+    @objc func initiateLogin(_ button: TransitionButton) {
+        
+        guard let email = emailTextField.text , emailTextField.text != "" else { return }
+        guard let password = passwordTextField.text , passwordTextField.text != "" else { return }
+        button.startAnimation()
+        let qualityOfServiceClass = DispatchQoS.QoSClass.background
+        let backgroundQueue = DispatchQueue.global(qos: qualityOfServiceClass)
+        backgroundQueue.async(execute: { [weak self] in
+            
+        guard let strongSelf = self else { return }
+            
+            //Do Auth Service calls here
+            
+        })
+    }
+    
+    @objc func initiateSignupForm() {
+        let signupVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SignupVC") as! SignupVC
+        self.navigationController?.pushViewController(signupVC, animated: true)
     }
 
+}
+
+
+extension LoginVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case emailTextField:
+            passwordTextField.becomeFirstResponder()
+        default:
+            passwordTextField.resignFirstResponder()
+        }
+        return true
+    }
 }
--- Clerkie-challenge/Utilities/Extensions.swift
+++ Clerkie-challenge/Utilities/Extensions.swift
@@ -6,4 +6,66 @@
 //  Copyright © 2018 Antonio Chan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIFont {
+    class func avenirBookFontOfSize(size: CGFloat) -> UIFont {
+        return UIFont(name: "Avenir-Book", size: size)!
+    }
+    class func avenirMediumFontOfSize(size: CGFloat) -> UIFont {
+        return UIFont(name: "Avenir-Medium", size: size)!
+    }
+    class func avenirMediumObliqueFontOfSize(size: CGFloat) -> UIFont {
+        return UIFont(name: "Avenir-MediumOblique", size: size)!
+    }
+    class func avenirNextRegularFontOfSize(size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-Regular", size: size)!
+    }
+    class func avenirNextDemiBoldFontOfSize(size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-DemiBold", size: size)!
+    }
+}
+
+extension UIColor {
+    convenience init(hexString: String, alpha: CGFloat = 1.0) {
+        let hexString: String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let scanner = Scanner(string: hexString)
+        if (hexString.hasPrefix("#")) {
+            scanner.scanLocation = 1
+        }
+        var color: UInt32 = 0
+        scanner.scanHexInt32(&color)
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
+    }
+    func toHexString() -> String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        return String(format:"#%06x", rgb)
+    }
+}
+
+extension UIColor {
+    struct FlatColor {
+        struct Gray {
+            static let WhiteSmoke = UIColor(hexString: "#fbfafc")
+        }
+        struct Yellow {
+            static let PastelYellow = UIColor(hexString: "#fdfd96")
+        }
+        
+        struct Blue {
+            static let PastelBlue = UIColor(hexString: "#aec6cf")
+        }
+    }
+}
